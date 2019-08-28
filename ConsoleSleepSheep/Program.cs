8e80f9bd89f4a6d52b9d26a56d774af4d2d65b06
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-08-28 16:36:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleSleepSheep/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleSleepSheep {
+    class Program {
+        public static void CountSheeps() { 
+            bool[] sheeps = { true, false, true };
+            var result =  sheeps.Count(s => s);
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args) {
+            CountSheeps();
+            Console.ReadKey();
+        }
+    }
+}
